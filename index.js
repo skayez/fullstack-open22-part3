2a51b37c99297ea49b9e0d1adcaf99ee5a52dc63
skayez <fyr121092@gmail.com>
2022-04-13 20:13:16 +0300

    Tehtävien palautus

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status - :response-time ms :body'));
