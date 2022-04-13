38ee33780bcbc589ff43cb84a085eaa31552dac7
skayez <fyr121092@gmail.com>
2022-04-13 16:38:14 +0300

    Tehtävien palautus

---
+++ index.js
@@ -0,0 +1,91 @@
+const { response } = require('express')
+const cors = require('cors')
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status - :response-time ms :body'));
+
+let persons = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456",
+    },
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+    },
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345",
+    },
+    {
+      id: 4,
+      name: "Mary Poppendick",
+      number: "39-23-6423122",
+    }
+  ]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(value => value.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(value => value.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number is missing'
+    })
+  }
+
+  if (persons.find(value => value.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
